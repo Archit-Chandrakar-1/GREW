e8a59e520383f57126988af9298031c9ac523532
Archit Chandrakar <archit1chandrakar@gmail.com>
2025-06-05 11:00:31 +0530

    commit

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,8 +4,8 @@
  */
 
 // Mock API key (in a real app, this would be securely stored)
-const API_KEY = '21ae0f3471f4b5be594316f25ba7f937';
-const BASE_URL = 'https://api.marketstack.com/v2';
+const API_KEY = 'demo';
+const BASE_URL = 'https://www.alphavantage.co/query';
 
 // Fetch stock data from Alpha Vantage API
 async function fetchStockData(symbol) {
